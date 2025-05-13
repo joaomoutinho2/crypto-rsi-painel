bebd53ee55da9b186ea9fcad99b20acff06e70ca
Joao Pedro Ramos Mouitnho <a56573@alunos.ipb.pt>
2025-05-13 22:07:04 +0100

    e

--- wsgi.py
+++
@@ -1,7 +0,0 @@
-import threading
-from bot.bot_rsi import app, iniciar_bot
-
-# Inicia o loop do bot em background
-threading.Thread(target=iniciar_bot, daemon=True).start()
-
-# O Gunicorn vai usar o objeto WSGI `app`
