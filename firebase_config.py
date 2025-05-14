d1b137fd90eb7c476da7b38d5571a154a19c3290
Joao Pedro Ramos Mouitnho <a56573@alunos.ipb.pt>
2025-05-14 13:07:22 +0100

    e

--- firebase_config.py
+++ firebase_config.py
@@ -4,7 +4,6 @@ import os
 import json
 
 def iniciar_firebase(usando_secrets=False, secrets=None):
-    # Verifica se o Firebase já foi inicializado
     if not firebase_admin._apps:
         if usando_secrets and secrets:
             # 🔐 Modo Streamlit (secrets.toml)
@@ -13,15 +12,15 @@ def iniciar_firebase(usando_secrets=False, secrets=None):
                 firebase_dict["private_key"] = firebase_dict["private_key"].replace("\\n", "\n")
             cred = credentials.Certificate(firebase_dict)
         else:
-            # 🔐 Modo Render (usando variável de ambiente FIREBASE_JSON)
-            firebase_json = os.environ["FIREBASE_JSON"]
+            # 🔐 Modo Render (usando FIREBASE_JSON no ambiente)
+            firebase_json = os.environ.get("FIREBASE_JSON")
+            if not firebase_json:
+                raise RuntimeError("⚠️ FIREBASE_JSON não está definida no ambiente!")
             firebase_dict = json.loads(firebase_json)
             if "\\n" in firebase_dict["private_key"]:
                 firebase_dict["private_key"] = firebase_dict["private_key"].replace("\\n", "\n")
             cred = credentials.Certificate(firebase_dict)
 
-        # Inicializa a app Firebase
         firebase_admin.initialize_app(cred)
 
-    # Retorna o cliente Firestore
     return firestore.client()
